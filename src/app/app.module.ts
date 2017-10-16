2868083900a0bb4d0296ac2934e302ebe4236cae
Isaac Tullis <isaacjtullis@gmail.com>
2017-10-16 09:47:27 -0500

    building angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+// The @ symbol tells us this is a decorator
+// A decorator allows us to easily extend a class
+
 @NgModule({
   declarations: [
     AppComponent
